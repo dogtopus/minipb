a8fd946936b80a235a4e19d6689224d4cc17d1e1
dogtopus <dogtopus@users.noreply.github.com>
2022-09-13 13:51:45 -0300

    Switch to semver and bump version to 1.0.0

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@
 from setuptools import setup
 
 setup(name='minipb',
-      version='0.1',
-      description='Mini Protobuf library.',
+      version='1.0.0',
+      description='Lightweight Protocol Buffer serialize/deserialize library.',
       author='dogtopus',
       py_modules=['minipb']
 )
