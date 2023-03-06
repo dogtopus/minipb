7029afd948b5a9cabce7b82c8fab5f7e69242e22
dogtopus <dogtopus@users.noreply.github.com>
2023-03-05 21:52:02 -0400

    Move to pyproject.toml and replace setup.py with a shim

--- setup.py
+++ setup.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python3
 from setuptools import setup
 
-setup(name='minipb',
-      version='1.0.0',
-      description='Lightweight Protocol Buffer serialize/deserialize library.',
-      author='dogtopus',
-      py_modules=['minipb']
-)
+if __name__ == '__main__':
+    setup()
